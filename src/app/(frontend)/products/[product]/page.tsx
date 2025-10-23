aa780abaf4eff17214e927b7e3a53f496f7ab94e
tafadzwa-mds <tafadzwa@moderndaystrategy.com>
2025-10-23 11:59:12 +0200

    fix - build errors

--- src/app/(frontend)/products/[product]/page.tsx
+++ src/app/(frontend)/products/[product]/page.tsx
@@ -1,13 +1,9 @@
 import React from 'react'
 
-export default function ProductPage({ params }) {
-  const capitalizeFirstLetter = (str: string) => {
-    return str.charAt(0).toUpperCase() + str.slice(1)
-  }
-
+export default function ProductPage({ params }: any) {
   return (
     <div style={{ padding: '4rem 2rem', minHeight: '60vh' }}>
-      <h1>{capitalizeFirstLetter(params.product)}</h1>
+      <h1>{params.product}</h1>
       <p>This page is under construction.</p>
     </div>
   )
